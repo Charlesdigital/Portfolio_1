33a2d34377a1d53843e0041a48ec859a18862d31
Charlese <charles.easton123@gmail.com>
2022-10-25 00:39:09 -0400

    add toggle menu logic

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,21 +1,22 @@
+import {useState} from 'react';
 import { Link } from 'react-router-dom'
 import "./navbar.css";
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaTimes, FaToggleOff } from 'react-icons/fa'
 // import LogoC from './assets/icons/c.svg'
 import Logo from '../../assets/icons/celogo.png'
 
 function Navbar() {
-// const [nav, setNav] = useState(false)
-// const handleClick = () => setNav(!nav)
+const [nav, setNav] = useState(false)
+const handleClick = () => setNav(!nav)
   return (
 <div>
-<nav>
         {/* <Link classNameName='logo' to='/'>
           <img src = {LogoC} alt="logo" />
         </Link> */}
         {/* <Link classNameName='logo' to='/'>
           <img src = {test} alt="logo" />
         </Link> */}
+        <div className= "nav">
 
         <img className="top-logo" src = {Logo} alt="logo" />
         <div>
@@ -27,21 +28,22 @@ function Navbar() {
         <li className="menu">Contact</li>
         </ul>
         </div>
-</nav>
-{/* hamburger */}
-<div className= "hidden">
-<FaBars/>
+        <div onClick = {handleClick} className= "burger">
+          {!nav ? <FaBars/> : <FaTimes />}
 </div>
+        </div>
+
+{/* hamburger */}
+
 
 {/* Mobile view */}
-<ul className="hidden">
+<ul onClick = {handleClick} className= {!nav ? "nopop" : "pop"}>
         <li className="menu">Home</li>
         <li className="menu">About</li>
         <li className="menu">Tech Stack</li>
         <li className="menu">Projects</li>
         <li className="menu">Contact</li>
         </ul>
-
 </div>
   )
 }
