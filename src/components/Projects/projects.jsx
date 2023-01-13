62bd0d29a92f6783a5c0b4963706fa8d626983e0
Charlese <charles.easton123@gmail.com>
2023-01-13 05:11:31 -0500

    add project container

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -4,14 +4,63 @@ import "./projects.css";
 function Projects() {
 
     return (
-<div className="projects">
-    <div> Yo Low Trips</div>
-    <div> WikiMaps </div>
-    <div> Interview Scheduler </div>
-    <div> Tweeter </div>
+<section className="projects">
+    <div className="projectContainer">
+    <h1>Yo Low Trips</h1>
+    <p> A full stack application to find flight deals</p>
+    <ul>
+    <li>HTML5</li>
+    <li>Javascript</li>
+    <li>React</li>
+    <li>Flight Data API</li>
+   </ul>
+   <a href="https://hotartistapp.netlify.app/" target="_blank">Live</a>
+   <ul>
+   </ul>
+   <a href="https://github.com/Charlesdigital/Yo-Low-Trips" target="_blank">Repo</a>
+   <ul>
+    <li></li>
+   </ul>
+    </div>
+
+    <div className="projectContainer">
+    <h1>Wiki Maps</h1>
+    <p> </p>
+    <ul>
+    <li>HTML5</li>
+    <li>Javascript</li>
+    <li>CSS</li>
+    <li>Flight Data API</li>
+   </ul>
+   <a href="https://hotartistapp.netlify.app/" target="_blank">Live</a>
+   <ul>
+   </ul>
+   <a href="https://github.com/araff-16/Wiki-Maps/tree/master">Repo</a>
+   <ul>
+    <li></li>
+   </ul>
+    </div>
+    <div className="projectContainer">
+    <h1>Scheduler</h1>
+    <p> A full stack application to find flight deals</p>
+    <ul>
+    <li>HTML5</li>
+    <li>Javascript</li>
+    <li>React</li>
+    <li>Flight Data API</li>
+   </ul>
+   <a href="https://hotartistapp.netlify.app/" target="_blank">Live</a>
+   <ul>
+   </ul>
+   <a href="https://github.com/Charlesdigital/scheduler" target="_blank">Repo</a>
+   <ul>
+    <li></li>
+   </ul>
+    </div>
+   
 
 
-</div>
+</section>
     )
 }
   export default Projects
