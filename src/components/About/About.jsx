212069e73425737ce4bda94a678a207cd5dcc091
Charlese <charles.easton123@gmail.com>
2023-02-02 11:38:46 -0500

    edit abbout me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,8 +13,7 @@ function About() {
         <img className="Charles" src = {me} alt="Charles" />
         <div className="summary">
     <div className="text-content">
-    <p > I'm a Full-Stack Developer with a business background. My curiosity for continuous learning and tinkering with technology lead me to be a Developer through
-    online resources. Experience working with large ERP solutions such as PeopleSoft and SAP in an accounting function. I enjoy learning the fundamentals of programming
+    <p > I'm a Full-Stack Developer with a business background. My curiosity for continuous learning and tinkering with technology lead me to be a Developer. I have experience working with large ERP solutions such as PeopleSoft and SAP in an accounting function. I enjoy learning the fundamentals of programming
     and breaking down business problems into code. </p>
     <p>During my spare time I enjoy cycling and exploring the city. <MdOutlineElectricBike /></p>
     <p>I enjoy travelling and experiencing new places. <GiWorld /></p>
