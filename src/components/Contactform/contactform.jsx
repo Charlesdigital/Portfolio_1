579fcb1ead87278361eb7067079d80eb90fb146d
Charlese <charles.easton123@gmail.com>
2023-02-06 10:24:21 -0500

    make contact mobile resposnive

--- src/components/Contactform/contactform.jsx
+++ src/components/Contactform/contactform.jsx
@@ -9,7 +9,7 @@ function contactform() {
 <form className="box" action="https://formsubmit.co/charles.easton@mail.utoronto.ca" method="POST" >
 <div className= "header-info">
 <h2 className="skill-title">Contact Me </h2>
-<p> Please leave a note and I will get back to you within 48 hours.</p>
+<p className="note"> Please leave a note and I will get back to you within 48 hours.</p>
 
 </div>
 <div className="input-info">
@@ -25,7 +25,7 @@ function contactform() {
 </div>
 
 <div className="messageContainer">
-<textarea className="messageholder" placeholder="Message" cols="60" rows="25"></textarea>
+<textarea className="messageholder" placeholder="Message" cols="50" rows="20"></textarea>
 <div>
 {/* <img className="mail" src = {contact} alt="mail" /> */}
 
