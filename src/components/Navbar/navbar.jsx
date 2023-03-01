8cf9401275c5d4d20fb0a0b121a5f6d35423644c
Charlese <charles.easton123@gmail.com>
2023-02-28 23:45:49 -0500

    remove unused code in navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,33 +1,21 @@
 import { useState, useRef } from "react";
 import "./navbar.css";
 import { FaBars, FaTimes } from "react-icons/fa";
-import CE from "../../assets/icons/CE.png";
 
 import { Link } from "react-scroll";
 function Navbar() {
-	const navRef = useRef();
+  const navRef = useRef();
 
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle(
-			"responsive_nav"
-		);
-    }
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
   return (
     <div className="container">
-
-    <nav className="nav" ref={navRef}>
-    {/* <button onClick = {handleClick} className= "burger">
-          {!nav ? <FaBars className= "icon" size={50} onClick={showNavbar}/> :  <FaTimes  className="icon cross" size={50} onClick={showNavbar}/>}
-</button> */}
-      {/* <div className= "navbar"> */}
-      <a href = "https://velvety-marshmallow-639425.netlify.app/">
-            {/* <img className="top-logo" src={CE} alt="logo" /> */}
-          </a>
+      <nav className="nav" ref={navRef}>
         <ul className="list">
-
           <li className="menu">
             <Link to="homeHeader" smooth={true} duration={500}>
               Home
@@ -52,66 +40,22 @@ function Navbar() {
             </Link>
           </li>
 
-
-
-          <li className="menu" >
+          <li className="menu">
             <Link className="contact" to="box" smooth={true} duration={500}>
               Contact
             </Link>
           </li>
-
         </ul>
-
-          {/* <ul className ="nav-mobile"
-        >
-
-
-
-          <li className="menu2">
-            <Link to="box" smooth={true} duration={500}>
-              Home
-            </Link>
-          </li>
-
-          <li className="menu2">
-            <Link to="about-info" smooth={true} duration={500}>
-              About
-            </Link>
-          </li>
-
-          <li className="menu2">
-            <Link to="Skills" smooth={true} duration={500}>
-              Skills
-            </Link>
-          </li>
-
-          <li className="menu2">
-            <Link to="projects" smooth={true} duration={500}>
-              Projects
-            </Link>
-          </li>
-
-          <li className="menu2">
-            <Link to="box" smooth={true} duration={500}>
-              Contact
-            </Link>
-          </li>
-        </ul> */}
-        </nav>
-
-      </div>
+      </nav>
+    </div>
   );
 }
 
 export default Navbar;
-// className="mobile-list"
-          // onClick={handleClick}
-          // className={!nav ? "hide-menu" : "show-menu"}
 
-  {/* <div className="socials">
-<a href="https://www.linkedin.com/in/charles-easton1/" target="_blank" rel="noopener noreferrer" ><BsLinkedin size={50} color="#0072b1" /></a>
-<a href="https://github.com/Charlesdigital" target="_blank" rel="noopener noreferrer"><BsGithub size={50} color="white" /></a>
-</div> */}
+// className="mobile-list"
+// onClick={handleClick}
+// className={!nav ? "hide-menu" : "show-menu"}
 
 // import { useState } from "react";
 // import "./navbar.css";
